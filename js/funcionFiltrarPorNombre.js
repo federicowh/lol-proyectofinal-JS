bb04dba82948755f6d7e687ad44db58500139735
Federico Weimann <federicoweimannh@gmail.com>
2021-09-16 23:40:12 -0300

    Cambiamos el nombre de los archivos .JS para que sean mas lógicos

--- js/funcionFiltrarPorNombre.js
+++ js/funcionFiltrarPorNombre.js
@@ -1,9 +1,9 @@
 // //Función para buscar Campeón por H5:
 $(document).ready(function(){
     $('#filtro').on("keyup", function(){
-        const value = $(this).val().toLowerCase();
+        const valor = $(this).val().toLowerCase();
         $('.campeonesEnImagenes').filter(function(){
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+            $(this).toggle($(this).text().toLowerCase().indexOf(valor) > -1);
         });
     });
 });
